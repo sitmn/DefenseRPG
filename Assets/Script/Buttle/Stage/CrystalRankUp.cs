458f47ee047d082e21613759d198460431615199
S.T <>
2022-08-09 02:29:58 +0900

    水晶ランクアップ用機能　2割

---
+++ Assets/Script/Buttle/Stage/CrystalRankUp.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalRankUp : MonoBehaviour
+{
+    //ランクアップ用カウント
+    public int _rankUpCount;
+    [SerializeField]
+    //ランクアップまでのカウント
+    public int _rankUpMaxCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rankUpCount = 0;
+    }
+
+    public void UpdateManager(){
+        if(!RankUpCount()) return;
+
+        RankUp();
+    }
+
+    //◎ランクアップまでのカウント
+    private bool RankUpCount(){
+        bool _rankUpFlag = false;
+        _rankUpCount ++;
+
+        if(_rankUpFlag >= _rankUpMaxCount){
+            _rankUpCount = 0;
+            
+            _rankUpFlag = true;
+        }
+
+        return _rankUpFlag;
+    }
+
+    //水晶の
+    private int RankUp(int _nowRank){
+        _now
+    }
+
+}
