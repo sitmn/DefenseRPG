deef53b360e4b2c01219b233be098d6e6712caf4
sitimin <shotjpx375@gmail.com>
2022-09-13 03:08:38 +0900

    HPがMaxのときはHPBarが表示されないよう修正

--- Assets/Script/Buttle/UI/HPBar.cs
+++ Assets/Script/Buttle/UI/HPBar.cs
@@ -7,41 +7,47 @@ using UniRx.Triggers;
 
 public class HPBar : MonoBehaviour
 {
+    [SerializeField]
     //HPのスライダー
     private Slider _hpSlider;
     [SerializeField]
     //HPバーをセットする対象のTransform
     private Transform _targetTr;
+    [SerializeField]
+    //HPバーの座標
     private RectTransform _rect;
+    //HPバーのゲームオブジェクト
+    [SerializeField]
+    private GameObject _hpBar;
 
     public void AwakeManager(){
-        this._rect = this.gameObject.GetComponent<RectTransform>();
-        _hpSlider = this.gameObject.GetComponent<Slider>();
-        SetHPBarValue(1);
+        // this._rect = this.gameObject.GetComponent<RectTransform>();
+        // _hpSlider = this.gameObject.GetComponent<Slider>();
         CreateMoveHPBarStream();
+        SetHPBarValue(1);
     }
 
     //画面移動に合わせてHPBarの移動
     private void CreateMoveHPBarStream(){
         this.UpdateAsObservable().Subscribe((_) => {
-            SetPosition();
+            if(_hpBar.activeSelf == true) SetPosition();
         }).AddTo(this);
     }
 
     //HPバーを対象の上部にセット
     public void SetPosition(){
-        //if(MapManager.IsOutOfReference(MapManager.CastMapPos(this._targetTr.position))) return;
-
         Vector2 _pos = Camera.main.WorldToScreenPoint(this._targetTr.position + new Vector3(0, ConstManager._hpBarPosOffsetY, ConstManager._hpBarPosOffsetZ));
         this._rect.position = _pos;
-        if(_rect.position.x < 100){
-Debug.Log(_rect.position + "A");
-        }
-        
     }
 
     //HPバーを更新
     public void SetHPBarValue(float _hpRaito){
+        if(_hpRaito < 1){
+            _hpBar.SetActive(true);
+        }
         _hpSlider.value = _hpRaito;
+        if(_hpRaito == 1){
+            _hpBar.SetActive(false);
+        }
     }
 }
